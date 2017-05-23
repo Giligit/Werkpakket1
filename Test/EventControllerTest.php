a8dacd4cae639e3219864253382ef38f34d24df8
Dario <dario.noe@outlook.com>
2017-05-23 15:19:24 +0200

    phpunit doesnt work...

---
+++ Test/EventControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dario
+ * Date: 23/05/2017
+ * Time: 13:52
+ */
+
+namespace Test;
+require_once(__DIR__.'/../vendor/autoload.php');
+require_once (__DIR__.'/../phpunit-6.1.4.phar/phpunit/framework/testcase.php');
+use Controller\EventController;
+
+
+class EventControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+
+}
+?>
